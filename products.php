03f5c797e8af7f69e8d570470b4fb07fac9319a5
glebkalachev <megas781@gmail.com>
2019-05-05 12:37:50 +0400

    Initial Commit. Have initial layout for products

---
+++ products.php
@@ -0,0 +1,166 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Продукты - Молоко.ру</title>
+</head>
+<body>
+
+<div class="top-bar">
+    <div class="wr960">
+        <span class="top-bar-item">г. Москва, 5-ая Парковая 39</span>
+        <span class="top-bar-item">Тех. Поддержка: 8 (495) 228-31-31</span>
+    </div>
+</div>
+
+<header class="header">
+    <div class="wr960">
+        <a href="#"><img src="" alt="logo"></a>
+    </div>
+</header>
+
+<nav class="nav">
+    <div class="wr960">
+        <div class="nav-links">
+            <a href="#" class="nav-link">Главная</a>
+            <a href="#" class="nav-link">Товары</a>
+            <a href="#" class="nav-link">Продавцы</a>
+            <a href="#" class="nav-link">О сайте</a>
+            <a href="#" class="nav-link">F.A.Q</a>
+            <a href="#" class="nav-link">Контакты</a>
+        </div>
+
+        <a class="nav-cart" href="#">Корзина</a>
+    </div>
+</nav>
+
+<div class="breadcrumbs">
+    <a class="breadcrumb-item" href="">Главная</a>
+    <a class="breadcrumb-item" href="">Товары</a>
+</div>
+
+<h1 class="page-title">Товары</h1>
+
+<main class="content">
+    <div class="wr960">
+        <div class="search-form" action="" >
+            <div class="select-input">
+                <label for="locality">Населённый пункт:</label>
+                <select name="locality" id="locality">
+                    <option value="moskva">Москва</option>
+                    <option value="slusaryovo">Слюсарёво</option>
+                    <option value="kotelniky">Котельники</option>
+                    <option value="ustannoe">Устанное</option>
+                </select>
+            </div>
+            <div class="select-input">
+                <label for="locality">Сортировать:</label>
+                <select name="locality" id="locality">
+                    <option value="moskva">Сначала новые</option>
+                    <option value="slusaryovo">Сначала дорогие</option>
+                    <option value="kotelniky">Сначала дешевые</option>
+                </select>
+            </div>
+            <div class="search-input">
+                <input class="search-textbox" type="text" placeholder="Поиск по сайту...">
+                <input type="submit" value="Искать">
+            </div>
+        </div>
+        <div class="products">
+            <section class="product" style="border: 1px solid black">
+                <img class="product-image" src="" alt="image">
+                <span class="product-title">Молоко пастеризованное 3%, 1л</span>
+                <span class="product-desc">Козье молоко (пастеризованное) 0.5л. Белок 2.8% до 3.2%. Жирность...</span>
+                <div class="seller">
+                    <div class="seller-name"><span>Продавец:</span> <a href="#">Галиева Екатерина</a></div>
+                    <span class="seller-locality">Село Слюсарёво, МО</span>
+                </div>
+                <span class="product-price">230 руб.</span>
+                <div class="product-controls">
+                    <div class="product-quantity stepper">
+                        <span class="stepper-minus">-</span>
+                        <span class="stepper-number">1</span>
+                        <span class="stepper-plus">+</span>
+                    </div>
+                    <button class="add-to-cart-button">В корзину</button>
+                </div>
+            </section>
+            <section class="product" style="border: 1px solid black">
+                <img class="product-image" src="" alt="image">
+                <span class="product-title">Молоко пастеризованное 3%, 1л</span>
+                <span class="product-desc">Козье молоко (пастеризованное) 0.5л. Белок 2.8% до 3.2%. Жирность...</span>
+                <div class="seller">
+                    <div class="seller-name"><span>Продавец:</span> <a href="#">Галиева Екатерина</a></div>
+                    <span class="seller-locality">Село Слюсарёво, МО</span>
+                </div>
+                <span class="product-price">230 руб.</span>
+                <div class="product-controls">
+                    <div class="product-quantity stepper">
+                        <span class="stepper-minus">-</span>
+                        <span class="stepper-number">1</span>
+                        <span class="stepper-plus">+</span>
+                    </div>
+                    <button class="add-to-cart-button">В корзину</button>
+                </div>
+            </section>
+            <section class="product" style="border: 1px solid black">
+                <img class="product-image" src="" alt="image">
+                <span class="product-title">Молоко пастеризованное 3%, 1л</span>
+                <span class="product-desc">Козье молоко (пастеризованное) 0.5л. Белок 2.8% до 3.2%. Жирность...</span>
+                <div class="seller">
+                    <div class="seller-name"><span>Продавец:</span> <a href="#">Галиева Екатерина</a></div>
+                    <span class="seller-locality">Село Слюсарёво, МО</span>
+                </div>
+                <span class="product-price">230 руб.</span>
+                <div class="product-controls">
+                    <div class="product-quantity stepper">
+                        <span class="stepper-minus">-</span>
+                        <span class="stepper-number">1</span>
+                        <span class="stepper-plus">+</span>
+                    </div>
+                    <button class="add-to-cart-button">В корзину</button>
+                </div>
+            </section>
+            <section class="product" style="border: 1px solid black">
+                <img class="product-image" src="" alt="image">
+                <span class="product-title">Молоко пастеризованное 3%, 1л</span>
+                <span class="product-desc">Козье молоко (пастеризованное) 0.5л. Белок 2.8% до 3.2%. Жирность...</span>
+                <div class="seller">
+                    <div class="seller-name"><span>Продавец:</span> <a href="#">Галиева Екатерина</a></div>
+                    <span class="seller-locality">Село Слюсарёво, МО</span>
+                </div>
+                <span class="product-price">230 руб.</span>
+                <div class="product-controls">
+                    <div class="product-quantity stepper">
+                        <span class="stepper-minus">-</span>
+                        <span class="stepper-number">1</span>
+                        <span class="stepper-plus">+</span>
+                    </div>
+                    <button class="add-to-cart-button">В корзину</button>
+                </div>
+            </section>
+
+        </div>
+    </div>
+</main>
+
+<footer>
+    <div class="wr960">
+        <div class="links">
+            <a href="#" class="footer-link">Услуги</a>
+            <a href="#" class="footer-link">Условия сотридничества</a>
+            <a href="#" class="footer-link">Лицензия</a>
+            <a href="#" class="footer-link">О нас</a>
+            <a href="#" class="footer-link">F.A.Q</a>
+            <a href="#" class="footer-link">Контакты</a>
+        </div>
+        <span class="copyright">© 2019 ООО «Молочный Мир»</span>
+    </div>
+</footer>
+
+
+</body>
+</html>
