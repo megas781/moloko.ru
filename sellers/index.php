733e8e7290b924a845b511275e8bf76fddabc30c
glebkalachev <megas781@gmail.com>
2019-05-08 17:12:20 +0300

    seller layour realized

--- sellers/index.php
+++ sellers/index.php
@@ -11,7 +11,71 @@ $APP->includeHeaderWithParams($metainfo);
 
 ?>
 
-тут продают!!
+<div class="search-form" action="" >
+    <div class="select-input">
+        <label for="locality">Населённый пункт:</label>
+        <select name="locality" id="locality">
+            <option value="moskva">Москва</option>
+            <option value="slusaryovo">Слюсарёво</option>
+            <option value="kotelniky">Котельники</option>
+            <option value="ustannoe">Устанное</option>
+        </select>
+    </div>
+    <div></div>
+    <div class="search-input">
+        <input class="search-textbox" type="text" placeholder="Поиск по сайту...">
+        <input type="submit" value="Искать">
+    </div>
+</div>
+<div class="sellers">
+
+    <section class="seller">
+        <img class="seller-image" src="/images/seller1.png" alt="фото продавца">
+        <a class="seller-name" href="#">Галиева Екатерина</a>
+        <div class="seller-locality">Село Слюсарёво, МО</div>
+        <p class="seller-desc">В продаже имеется домашнее молоко, сыр, сметана. Мы кормим коров только экологически чистой пищей, что держит качество наших продуктов на высоком уровне...</p>
+        <div class="seller-detail-button-positioner">
+            <a href="#" class="detail-link blue-button">Подробнее</a>
+        </div>
+    </section>
+    <section class="seller">
+        <img class="seller-image" src="/images/seller1.png" alt="фото продавца">
+        <a class="seller-name" href="#">Галиева Екатерина</a>
+        <div class="seller-locality">Село Слюсарёво, МО</div>
+        <p class="seller-desc">В продаже имеется домашнее молоко, сыр, сметана. Мы кормим коров только экологически чистой пищей, что держит качество наших продуктов на высоком уровне...</p>
+        <div class="seller-detail-button-positioner">
+            <a href="#" class="detail-link blue-button">Подробнее</a>
+        </div>
+    </section>
+    <section class="seller">
+        <img class="seller-image" src="/images/seller1.png" alt="фото продавца">
+        <a class="seller-name" href="#">Галиева Екатерина</a>
+        <div class="seller-locality">Село Слюсарёво, МО</div>
+        <p class="seller-desc">В продаже имеется домашнее молоко, сыр, сметана. Мы кормим коров только экологически чистой пищей, что держит качество наших продуктов на высоком уровне...</p>
+        <div class="seller-detail-button-positioner">
+            <a href="#" class="detail-link blue-button">Подробнее</a>
+        </div>
+    </section>
+    <section class="seller">
+        <img class="seller-image" src="/images/seller1.png" alt="фото продавца">
+        <a class="seller-name" href="#">Галиева Екатерина</a>
+        <div class="seller-locality">Село Слюсарёво, МО</div>
+        <p class="seller-desc">В продаже имеется домашнее молоко, сыр, сметана. Мы кормим коров только экологически чистой пищей, что держит качество наших продуктов на высоком уровне...</p>
+        <div class="seller-detail-button-positioner">
+            <a href="#" class="detail-link blue-button">Подробнее</a>
+        </div>
+    </section>
+    <section class="seller">
+        <img class="seller-image" src="/images/seller1.png" alt="фото продавца">
+        <a class="seller-name" href="#">Галиева Екатерина</a>
+        <div class="seller-locality">Село Слюсарёво, МО</div>
+        <p class="seller-desc">В продаже имеется домашнее молоко, сыр, сметана. Мы кормим коров только экологически чистой пищей, что держит качество наших продуктов на высоком уровне...</p>
+        <div class="seller-detail-button-positioner">
+            <a href="#" class="detail-link blue-button">Подробнее</a>
+        </div>
+    </section>
+
+</div>
 
 
 <?php $APP->includeFooterWithParams(); ?>
