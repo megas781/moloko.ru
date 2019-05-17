36bf1b7314721916d12fd7f88edaae5aec123470
glebkalachev <megas781@gmail.com>
2019-05-17 21:54:49 +0300

    implemented determining selected products in cards

--- master/base.js
+++ master/base.js
@@ -1,4 +1,4 @@
-// localStorage.clear();
+//достать данные корзины
 function getCart() {
     if (JSON.parse(localStorage.getItem('cart'))) {
         return JSON.parse(localStorage.getItem('cart'));
@@ -6,22 +6,41 @@ function getCart() {
         return {};
     }
 }
+//проверить наличие товара в корзине
+function checkItemAt(product_id) {
+    if (getCart()[product_id]) {
+        return true;
+    } else {
+        return false;
+    }
+}
+//добавить товар в корзину
 function addItem(product_id, quantity) {
     let array =  getCart();
     array[product_id] = quantity;
     localStorage.setItem('cart', JSON.stringify(array));
 }
+//удалить товар из корзины
 function removeItem(product_id) {
     let array =  getCart();
     array[product_id] = undefined;
     localStorage.setItem('cart', JSON.stringify(array));
 }
 
+//Определение уже лежащих в корзине товаров при загрузке страницы
+document.querySelectorAll('.product-card').forEach(function (product) {
+    let addToCardButton = product.querySelector('.add-to-cart-button');
+    let product_id = product.querySelector('.product_id_holder').value;
+
+    if (checkItemAt(product_id)) {
+        addToCardButton.classList.add('tapped');
+        addToCardButton.textContent = 'В корзине';
+    }
+});
 
 
 
-
-
+//Функционал кнопок stepper'a
 document.querySelectorAll('.stepper').forEach(function (value, key, parent) {
 
     let minusButton = value.querySelector('.stepper-minus');
@@ -39,11 +58,8 @@ document.querySelectorAll('.stepper').forEach(function (value, key, parent) {
 });
 
 
-//Включение "tapped" для кнопки "В корзину", где это надо
-
-
-//Привязка к "В корзину" для карточки товара
-document.querySelectorAll('.product').forEach(function (product, key, parent) {
+//Функционал кнопки "В корзину" для карточки товара
+document.querySelectorAll('.product-card').forEach(function (product, key, parent) {
 
     let productId = product.querySelector('.product_id_holder').value;
     let productNumber = product.querySelector('.stepper-number');
