8044879c4ffa91ba571a0bc8bef17fa5e1aed59a
glebkalachev <megas781@gmail.com>
2019-05-18 10:50:09 +0300

    product detail page layout changed

--- products/detail/index.php
+++ products/detail/index.php
@@ -6,6 +6,7 @@ $product = $APP->getProductById($_GET['product_id']);
 
 $metainfo = [
     'style_path' => HTTP_ROOT . 'products/detail/product-detail.css',
+    'script_path' => HTTP_ROOT . 'products/detail/product-detail.js',
     'page_id' => 'products',
     'page_title' => $product['title']
 ];
@@ -23,13 +24,13 @@ echo '</pre>';
 
 ?>
 
-<section class="product">
+<section class="the-product">
 
-    <img class="product-image" src="<?php echo $product['image_url'] ?>" alt="image">
+    <img class="the-product__image" src="<?php echo $product['image_url'] ?>" alt="image">
 
-    <div class="product-info">
+    <div class="the-product__product-info">
         <h4>Описание</h4>
-        <p class="product-desc">Энергетическая ценность <?php echo $product['energy_value']?>, белки <?php echo $product['squirrels'] ?> г, жиры <?php echo $product['fats'] ?> г, углеводы <?php echo $product['carbohydrates'] ?> г<br><br>
+        <p class="the-product__product-desc">Энергетическая ценность <?php echo $product['energy_value']?>, белки <?php echo $product['squirrels'] ?> г, жиры <?php echo $product['fats'] ?> г, углеводы <?php echo $product['carbohydrates'] ?> г<br><br>
             Хранить при температуре <?php echo $product['storage_temperature_from'] ?> - <?php echo $product['storage_temperature_to'] ?>°C
             <br><br>
             Срок годности: <?php echo $product['shelf_live'] ?> дней <br><br>
@@ -38,27 +39,27 @@ echo '</pre>';
         <h4>Объем: <?php echo $product['volume'] ?> л</h4>
     </div>
 
-    <div class="product-controls">
+    <div class="the-product__controls">
         <div>
-            <div class="product-price"><?php echo $product['price'] ?> руб</div>
+            <div class="the-product__price"><?php echo $product['price'] ?> руб</div>
 
-            <div class="product-quantity stepper">
+            <div class="the-product__quantity stepper">
                 <span class="stepper-minus stepper-control">–</span>
                 <span class="stepper-number">1</span>
                 <span class="stepper-plus stepper-control">+</span>
             </div>
         </div>
 
-        <span class="add-to-cart-button blue-button">В корзину</span>
+        <span id="detail-product-add-to-cart-button" class="add-to-cart-button blue-button" productId="<?php echo $_GET['product_id'] ?>">В корзину</span>
     </div>
 </section>
 
 <h2>О продавце</h2>
 
-<section class="seller">
-    <img class="seller-avatar" src="<?php echo $product['avatar_url'] ?>" alt="">
-    <div class="seller-info">
-        <a class="seller-name-link" href="/sellers/detail/?seller_id=<?php echo $product['seller_id'] ?>"><h2><?php echo $product['surname'] . ' ' . $product['name'] ?></h2></a>
+<section class="product-detail-seller">
+    <img class="product-detail-seller-avatar" src="<?php echo $product['avatar_url'] ?>" alt="">
+    <div class="product-detail-seller-info">
+        <a class="product-detail-seller-name-link" href="/sellers/detail/?seller_id=<?php echo $product['seller_id'] ?>"><h2><?php echo $product['surname'] . ' ' . $product['name'] ?></h2></a>
         <table>
             <tr>
                 <td>Телефон:</td>
@@ -99,4 +100,9 @@ echo '</pre>';
     }
 </script>
 
-<?php $APP->includeFooterWithParams(); ?>
+<h2 class="other-products-title">Другие товары</h2>
+<div class="products">
+    <?php $APP->printThreeRandomProducts() ?>
+</div>
+
+<?php $APP->includeFooterWithParams($metainfo); ?>
