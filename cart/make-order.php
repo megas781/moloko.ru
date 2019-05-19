c2844661f3742d27cf702ae820ea92e863bc8807
glebkalachev <megas781@gmail.com>
2019-05-19 15:11:19 +0300

    make-order.php script implemented

--- cart/make-order.php
+++ cart/make-order.php
@@ -1,4 +1,7 @@
 <?php
 
-
-header('Location: http://moloko.glebkalachev.ru');
+if (isset($_POST['cartSize']) and $_POST['cartSize'] > 0) {
+    header('Location: http://moloko.glebkalachev.ru/cart/order/');
+} else {
+    header('Location: http://moloko.glebkalachev.ru/cart/');
+}
