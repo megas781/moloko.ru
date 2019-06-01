c91f8571f15ad43809942eafc36acaba719d5838
glebkalachev <megas781@gmail.com>
2019-06-01 14:07:00 +0300

    fixed logic of lastCategory. Now it fetched value from the URL

--- products/products.js
+++ products/products.js
@@ -1,13 +1,28 @@
-let lastCategory = 'all';
-let lastSort = 'new';
+let url = new URL(location.href);
 
+let lastCategory;
+if (url.searchParams.get('category')) {
+    lastCategory = url.searchParams.get('category');
+} else {
+    lastCategory = 'all';
+}
+
+let lastSort;
+if (url.searchParams.get('sort')) {
+    lastSort = url.searchParams.get('sort');
+} else {
+    lastSort = 'new';
+}
+
+//Обновление станицы при изменении категории
 document.getElementById('category').addEventListener('change', function () {
     let categoryString = this.options[this.selectedIndex].value;
-    // console.log(categoryString);
-    if (lastCategory !== categoryString) {
+
+    //Эту проверку можно не делать, т.к. событие не обрабатывается, если параметр остался тем же
+    // if (lastCategory !== categoryString) {
         location.assign('http://' + location.hostname + `/products?category=${categoryString}&sort=${lastSort}`);
         lastCategory = categoryString;
-    }
+    // }
 });
 
 
