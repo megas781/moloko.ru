737ca04456dddabd83b5026fa99ad2301ff077d0
glebkalachev <megas781@gmail.com>
2019-06-02 18:51:07 +0300

    fonts now are loaded from the server instead of google fonts

--- master/footer.php
+++ master/footer.php
@@ -17,11 +17,11 @@ if (isset($params['script_path'])) {
 <footer class="footer">
     <div class="wr960">
         <div class="footer-links">
-            <a href="/about/" class="footer-link">Услуги</a>
-            <a href="/about/" class="footer-link">Условия сотридничества</a>
-            <a href="/about/" class="footer-link">Лицензия</a>
-            <a href="/about/" class="footer-link">О нас</a>
-            <a href="/faq/" class="footer-link">F.A.Q</a>
+<!--            <a href="/about/" class="footer-link">Услуги</a>-->
+<!--            <a href="/about/" class="footer-link">Условия сотридничества</a>-->
+<!--            <a href="/about/" class="footer-link">Лицензия</a>-->
+            <a href="/" class="footer-link">О нас</a>
+            <a href="/faq/" class="footer-link">Вопросы</a>
             <a href="/contacts/" class="footer-link">Контакты</a>
         </div>
         <span class="copyright">© 2019 команда «Римма и Веберы». Права не защищены. Сайт является сугубо учебным макетом и не осуществляет реальной коммерческой деятельности.</span>
